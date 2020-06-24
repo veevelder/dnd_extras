d3dc32ecacb297535f8242f3f7b38541defbd418
matt <veevelder@gmail.com>
2020-06-24 12:30:46 -0600

    removed uneeded items

--- scripts/book_event.js
+++
@@ -1,8 +0,0 @@
-function book_show_page(e, l) {
-	e.stopPropagation()
-	$(".chapter-section").hide()
-	let content = $($(l).attr('href'))
-	content.find(".chapter-section").show()
-	content.parents(".chapter-section").show()
-	content.show()
-}
