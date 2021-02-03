7666f52698dddce9d69565c2a84e933e5b7a7342
matt <veevelder@gmail.com>
2021-02-03 14:26:47 -0700

    added macro folder

---
+++ macros/raven_buddy.js
@@ -0,0 +1,58 @@
+(async () => {
+	const raven_name = "Royal";
+	let token = null;
+	if(game.user.isGM) {
+		if(canvas.tokens.controlled.length == 1) {
+			token = canvas.tokens.controlled[0];
+		}
+		else {
+			return ui.notifications.error("Only select one token at a time!");
+		}
+	}
+	else {
+		token = canvas.tokens.placeables.find(a => a.name === game.user.charname)
+	}
+	if(token) {
+		let target = new Set();
+		target.add(token);
+		//let targetScene = game.scenes.active;
+		let item = token.actor.effects.find(a => a.data.label == "Sentinel Raven");
+
+		if (item.data.disabled === false) {
+			//raven is on token set raven to flying
+			await token.actor.updateEmbeddedEntity("ActiveEffect", {"_id": item.data._id,  "disabled" : true});
+			await Summoner.placeAndSummon(
+			  token.actor,
+			  raven_name,
+			);
+			// Update Token
+			token.update({
+				vision: true,
+				dimSight: 0,
+				brightSight: 0,
+				dimLight: 0,
+				brightLight:  0,
+				lightAngle: 360
+			});
+			ChatMessage.create({content: `${token.actor.name}'s raven ${raven_name} now flies freely`})
+		}
+		else if (item.data.disabled === true){
+			//raven is flying have it mount back on player find all just in case
+			await token.actor.updateEmbeddedEntity("ActiveEffect", {"_id": item.data._id,  "disabled" : false});
+			await Summoner.dismiss("Royal");
+			//update token
+			token.update({
+				vision: true,
+				dimSight: 30,
+				brightSight: 0,
+				dimLight: 0,
+				brightLight:  0,
+				lightAngle: 360
+			});	
+			ChatMessage.create({content: `${token.actor.name}'s raven ${raven_name} now sits on thier shoulder`})
+		}
+	}
+	else {
+		return ui.notifications.error("No Token Selected");
+	}
+})();
